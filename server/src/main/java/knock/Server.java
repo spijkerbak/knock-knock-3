5dc82d36429b7db733993bb19176dcee7255ab27
Frans <frans@spijkerman.nl>
2023-02-09 13:01:04 +0100

    first commit

---
+++ server/src/main/java/knock/Server.java
@@ -0,0 +1,48 @@
+package knock;
+
+import knock.common.KConsole;
+import knock.common.KServerSocket;
+import knock.common.KSocket;
+
+import java.io.*;
+
+public class Server {
+    public static void main(String[] args) throws IOException {
+
+        int port = 4444;
+        KServerSocket serverSocket = new KServerSocket(port);
+        KConsole console = new KConsole();
+        while (true) {
+            KSocket socket = serverSocket.accept();
+            var handler = new Handler(console, socket);
+            handler.start();
+        }
+    }
+}
+
+class Handler extends Thread {
+
+    private KConsole console;
+    private KSocket socket;
+
+    Handler(KConsole console, KSocket socket) {
+        this.console = console;
+        this.socket = socket;
+    }
+
+    public void run() {
+        String inputLine, outputLine;
+        Protocol protocol = new Protocol();
+
+        outputLine = protocol.processInput(null);
+        socket.writeLine(outputLine);
+        while ((inputLine = socket.readLine()) != null) {
+            console.writeLine("Client: " + inputLine);
+            outputLine = protocol.processInput(inputLine);
+            socket.writeLine(outputLine);
+            if (outputLine.equals("Bye.")) {
+                break;
+            }
+        }
+    }
+}
